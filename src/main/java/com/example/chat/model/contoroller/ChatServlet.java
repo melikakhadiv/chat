d1d701ba998a3408905a3dd6690c9eaedc21db4d
marziyeh-sharifi <145792111+Marzi66@users.noreply.github.com>
2023-10-16 17:44:14 +0330

    servlet

--- src/main/java/com/example/chat/model/contoroller/ChatServlet.java
+++ src/main/java/com/example/chat/model/contoroller/ChatServlet.java
@@ -8,8 +8,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @WebServlet(name = "ChatServlet",urlPatterns = "/ChatServlet")
 public class ChatServlet extends HttpServlet{
@@ -18,6 +21,14 @@ public class ChatServlet extends HttpServlet{
         try {
                 String username = req.getSession().getAttribute("username").toString();
                 String message = req.getParameter("message");
+                String receiver=req.getParameter("reciver");
+                String sender=req.getParameter("sender");
+                String date=req.getParameter("date");
+                User user=User.builder().username(receiver).build();
+                User user1=User.builder().username(sender).build();
+                Chat chat=Chat.builder().message(message).date(LocalDateTime.parse(date)).receiver(user).
+                        sender(user1).build();
+
                 // WsServer.sendMessage(message, (Session) req.getSession().getAttribute("session"));
         }
 catch (Exception e) {
