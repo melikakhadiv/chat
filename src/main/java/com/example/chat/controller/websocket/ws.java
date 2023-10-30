7f0116794c416147d874fedb57093b96a5b7eee7
SaraZivary <144610838+SaraZivary@users.noreply.github.com>
2023-10-30 19:30:45 +0330

    Update ws.java

--- src/main/java/com/example/chat/controller/websocket/ws.java
+++ src/main/java/com/example/chat/controller/websocket/ws.java
@@ -1,59 +1,49 @@
 package com.example.chat.controller.websocket;
 
-import com.example.chat.controller.session.SessionManager;
 import com.example.chat.model.entity.Chat;
 import com.example.chat.model.util.HTMLFilter;
-import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 @ServerEndpoint(value = "/chat", encoders = MessageModelEncoder.class, decoders = MessageModelDecoder.class, configurator = GetHttpSessionConfigurator.class)
 public class ws {
-//    private static Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
+    private static Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
 
 
-    @OnMessage
-    public String onMessage(Session session, Chat chat) throws EncodeException, IOException {
+        @OnMessage
+    public String onMessage(Session session , Chat chat) throws EncodeException, IOException {
         System.out.println("message : " + chat);
-//        for (Session s : sessions) {
-//                s.getBasicRemote().sendObject(chat);
-//
-//        }
+        for (Session s : sessions) {
+                s.getBasicRemote().sendObject(chat);
+
+        }
         return chat.getMessage();
     }
 
 
+
     @OnOpen
-    public void onOpen(Session session) {
-        System.out.println("ws opened: " + session.getId());
-        HttpSession httpSession = (HttpSession) session.getUserProperties()
-                .get(HttpSession.class.getName());
-        SessionManager.addWebSocketSession(httpSession, session);
+    public void  onOpen(Session session){
+        System.out.println("ws opend: " + session.getId());
+        sessions.add(session);
     }
 
 
-    @OnClose
-    public void onClose(Session session) {
+        @OnClose
+    public void  onClose(Session session){
         System.out.println("ws closed: " + session.getId());
-        SessionManager.invalidateHttpSession((HttpSession) session.getUserProperties()
-                .get(HttpSession.class.getName()), session);
+        sessions.remove(session);
     }
 
 
-    public static void send(HttpSession httpSession, String msg) throws IOException {
-        SessionManager.getWebSocketSession(httpSession).getBasicRemote().sendText(msg);
+    @OnError
+    public void onError(Throwable t) throws Throwable {
     }
 
-    public static void broadcast(String message) throws IOException {
-        for (Map<HttpSession, Session> webSocketSession : SessionManager.getWebSocketSessions()) {
-            webSocketSession.get("session").getBasicRemote().sendText(message);
-        }
-    }
 
 }
