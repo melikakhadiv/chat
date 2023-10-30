946c54394d7cd2ceff21789b6e476829d4e3785e
SaraZivary <144610838+SaraZivary@users.noreply.github.com>
2023-10-30 13:34:13 +0330

    loginservlet

---
+++ src/main/java/com/example/chat/model/controller/LoginServlet.java
@@ -0,0 +1,46 @@
+package com.example.chat.model.controller;
+
+import com.example.chat.model.entity.User;
+import com.example.chat.model.service.UserService;
+import jakarta.security.enterprise.credential.Password;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.inject.Inject;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.openqa.selenium.UsernameAndPassword;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/Login")
+public class LoginServlet extends HttpServlet {
+
+    @Inject
+    private UserService userService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String username= req.getParameter("username");
+            String password=req.getParameter("password");
+            User user = userService.login(username,password);
+            req.getSession().setAttribute("username",user.getUsername());
+            resp.sendRedirect("/"+user.getRole()+"/panel.jsp");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            req.getParameter("logout");
+            req.getSession().invalidate();
+        }
+        catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
