8596865c7805212c5303c5197a79b7d8c05d14fd
SaraZivary <144610838+SaraZivary@users.noreply.github.com>
2023-10-31 15:28:34 +0330

    update userroles service

--- src/main/java/com/example/chat/model/service/UserRolesService.java
+++ src/main/java/com/example/chat/model/service/UserRolesService.java
@@ -1,12 +1,15 @@
 package com.example.chat.model.service;
 
 
+import com.example.chat.model.entity.User;
 import com.example.chat.model.entity.UserRoles;
 import com.example.chat.model.service.impl.ServiceImpl;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -18,28 +21,45 @@ public class UserRolesService implements ServiceImpl<UserRoles,String> {
     private EntityManager entityManager;
 
     @Override
+    @Transactional
+
     public UserRoles save(UserRoles userRoles) throws Exception {
         entityManager.persist(userRoles);
         return userRoles;
     }
 //todo: complete services
     @Override
+    @Transactional
+
     public UserRoles edit(UserRoles userRoles) throws Exception {
-        return null;
+        entityManager.merge(userRoles);
+        return userRoles;
     }
 
     @Override
-    public UserRoles remove(String id) throws Exception {
-        return null;
+    @Transactional
+
+    public UserRoles remove(String username) throws Exception {
+      UserRoles userRoles= entityManager.find(UserRoles.class,username);
+        entityManager.remove(username);
+        return userRoles;
     }
 
     @Override
+    @Transactional
+
     public List<UserRoles> findAll() throws Exception {
-        return null;
+
+        Query query = entityManager.createQuery("select oo from userrolesEntity oo");
+        return query.getResultList();
     }
 
     @Override
-    public UserRoles findById(String id) throws Exception {
-        return null;
+    @Transactional
+
+    public UserRoles findById(String username) throws Exception {
+        UserRoles userRoles= entityManager.find(UserRoles.class,username);
+        return userRoles;
     }
 }
+
